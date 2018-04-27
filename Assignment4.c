1bd6ec91c21de91b639fa36ae392a11eddcb3671
Fatimafaz <38763207+Fatimafaz@users.noreply.github.com>
2018-04-27 17:21:58 +0500

    Update Assignment4.c

--- Assignment4.c
+++ Assignment4.c
@@ -2,32 +2,28 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-
+//Declaration
 
 int array[1000];
-
 int sum=0;
 int start=0;
 
-
+//Function adding the 100 elements of the array 
 
 int sumfunc(void)
 {
-	int starting=  start;
-	int ending= start+99;
+	int starting=  start;  //initialized with value globally declared, and incremented in main
+	
+	int ending= start+99;  //ending will be 100th element from the starting point
+	
 	for(int i=starting; i<ending; i++)
 	
 	{
-		sum=array[i]+sum;
+		sum=array[i]+sum; //each sum having result of 100 additions
 	}
 	
 	return sum;
 }
-
-
-
-int totsum=0;
-
 
 
 pthread_t thread
@@ -36,17 +32,19 @@ void main()
 
 {
 	
+	int totsum=0;  //for main function, to store overall result of the summation
 	
 	for (int i=0 ; i<1000; i++)
- {
-	array[i]=1;
- }
+         {
+	       array[i]=1; //initializing array
+         }
 
 
 	for(int i=0;i<10;i++)
 	{
+		
 	pthread_create(&thread,NULL,sumfunc,NULL);
-	start=start+100;
+	start=start+100;                                 
 	
 	pthread_join(&thread,int sum1);
 
